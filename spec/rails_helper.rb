1b8f519bfef8c69fbdd7d026617d1a44ff07914d
Eugene Zaretskiy <ujinius@gmail.com>
2020-04-07 10:17:53 +0300

    remoove ide folder and move vcr fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,9 +9,9 @@ require 'rspec/rails'
 require 'capybara/rails'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  # config.filter_sensitive_data('<SOME_PLACEHOLDER>') { 'sensitive data' }
+  config.filter_sensitive_data('<USER-AGENT>') { 'Github-Search-Global-App' }
   config.configure_rspec_metadata!
 end
 
